b25bfe6308c0236cb7e25ccfccaeac398a48bb03
Colin Carroll <colcarroll@gmail.com>
2019-12-18 20:25:23 -0500

    Use an equal aspect ratio on all dots

--- quantile_dotplot/quantile_dotplot.py
+++ quantile_dotplot/quantile_dotplot.py
@@ -1,7 +1,6 @@
 """Main functions in library."""
 import matplotlib.pyplot as plt
-from matplotlib.collections import PatchCollection
-from matplotlib.patches import Ellipse
+from matplotlib.ticker import MaxNLocator
 import numpy as np
 
 __all__ = ["compute_ntiles", "ntile_dotplot"]
@@ -70,23 +69,33 @@ def ntile_dotplot(data, dots=10, hist_bins="auto", **kwargs):
         The collection of artists added to the axes.
 
     """
-    centers, counts = compute_ntiles(data, dots=dots, hist_bins=hist_bins)
-    bin_width = centers[1] - centers[0]
-    xlims = centers.min() - bin_width, centers.max() + bin_width
-    y_ratio = (xlims[1] - xlims[0]) / (counts.max() - 1)
-    ylims = (0.5, counts.max() + 1.0)
-    centers = np.repeat(centers, counts)
-    counts = np.concatenate([np.arange(1, j + 1) for j in counts])
+    centers_, counts = compute_ntiles(data, dots=dots, hist_bins=hist_bins)
+    centers = np.repeat(centers_, counts)
+    counts = np.concatenate([np.arange(0.5, j + 0.5) for j in counts])
 
     axis = kwargs.pop("ax", None)
     if axis is None:
         axis = plt.gca()
-    patches = [
-        Ellipse(center, width=min(bin_width, y_ratio), height=1)
-        for center in zip(centers, counts)
-    ]
-    circs = PatchCollection(patches, **kwargs)
-    circs = axis.add_collection(circs)
-    axis.set_ylim(*ylims)
-    axis.set_xlim(*xlims)
-    return circs
+    axis.figure.set_dpi(72.0)
+    kwargs.setdefault("edgecolor", "black")
+    kwargs.setdefault("linewidth", 1)
+
+    circs = axis.scatter(centers, counts, s=1, **kwargs)
+    axis.yaxis.set_major_locator(MaxNLocator(integer=True))
+
+    if len(centers_) <= len(axis.get_xticks()):
+        axis.set_xticks(centers_)
+    bin_width = centers_[1] - centers_[0]
+    axis.set_xlim(centers_[0] - bin_width, centers_[-1] + bin_width)
+
+    axis.set_ylim(0, counts.max() + 1)
+    diff = axis.transData.transform([(0, 1), (bin_width, 0), (0, 0)])
+    x_scale = max(diff[0] - diff[-1])
+    y_scale = max(diff[1] - diff[-1])
+    if x_scale > y_scale:
+        axis.set_ylim(0, (x_scale / y_scale) * (counts.max() + 1))
+        diff = axis.transData.transform([(0, 1), (bin_width, 0), (0, 0)])
+
+    size = max(diff[0] - diff[-1])
+    circs.set_sizes([size ** 2 for _ in centers])
+    return axis
